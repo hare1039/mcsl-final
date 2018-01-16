3234efe21a36f2c3e75b049c2d11c03353678cad
hare1039 <hare39393@gmail.com>
2018-01-17 02:00:12 +0800

    Double buffer failed

--- main.cpp
+++ main.cpp
@@ -1,48 +1,61 @@
 #include "mbed.h"
 #include "Serial.h"
 #include "PinNames.h"
-#include "xz.h"
 #include <queue>
 
 #define sample_freq 16000.0
 
-Serial bt(D10, D2, 9600);
-Serial pc(USBTX, USBRX, 9600);
+Serial bt(D10, D2, 230400);
+Serial pc(USBTX, USBRX, 230400);
 AnalogOut speaker(A2);
-Ticker tick;
+Ticker tick, tick2;
 
 std::queue<int> data;
 void audio_sample()
 {
 	if (not data.empty())
 	{
-//		pc.printf("%d ", data.front());
 		speaker.write_u16(data.front());
 		data.pop();
 	}
 }
 
 constexpr int buffer_size = 8*1024;
-unsigned char buffer[buffer_size];
-unsigned char buffer_next[buffer_size];
+unsigned char buffer0[buffer_size];
+bool buffer0_ok = false;
+unsigned char buffer1[buffer_size];
+bool buffer1_ok = false;
 
 
 int main()
 {
 	tick.attach(&audio_sample, 1.0 / sample_freq);
+	tick2.attach([](){pc.printf("data size: %d\r\n", data.size());}, 1.0 );
+	bt.read (buffer0, buffer_size, [](int x){ buffer0_ok = true; });
 	for(;;)
 	{
-		bt.read (buffer, buffer_size, [](int x)
-		    {
-			    bt.printf("reads\r\n");
-			    
-			    for (int i(0); i < buffer_size; i += 2)
-			    {
-				    int p = ((static_cast<int>(buffer_next[i]) << 8) | buffer_next[i+1]);
-				    pc.printf("%d ", p);
-			        data.push(p);
-			    }
-//			    pc.printf("data size %d\r\n", data.size());
-		    });
+		if (buffer0_ok)
+		{
+			pc.printf("buffer0_ok\r\n");
+			buffer0_ok = false;
+			for (int i(0); i < buffer_size; i += 2)
+			{
+				int p = ((static_cast<int>(buffer0[i]) << 8) | buffer0[i+1]);
+				data.push(p);
+			}
+			bt.read (buffer0, buffer_size, [](int x){ buffer0_ok = true; });
+		}
+		if (buffer1_ok)
+		{
+			pc.printf("buffer1_ok\r\n");
+			buffer1_ok = false;
+			for (int i(0); i < buffer_size; i += 2)
+			{
+				int p = ((static_cast<int>(buffer1[i]) << 8) | buffer1[i+1]);
+//x				pc.printf("%d ", p);
+				data.push(p);
+			}
+			bt.read (buffer0, buffer_size, [](int x){ buffer0_ok = true; });
+		}
 	}
 }
