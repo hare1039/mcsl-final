1e609a433738ad7563da9045f70bc0601c855f25
hare1039 <hare39393@gmail.com>
2018-01-17 00:06:10 +0800

    Save point before formally intreduce xz

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "mbed.h"
 #include "Serial.h"
 #include "PinNames.h"
+#include "xz.h"
 #include <queue>
 
 #define sample_freq 16000.0
 
 Serial bt(D10, D2, 9600);
-Serial pc(USBTX, USBRX, 115200);
+Serial pc(USBTX, USBRX, 9600);
 AnalogOut speaker(A2);
 Ticker tick;
 
@@ -21,21 +22,24 @@ void audio_sample()
 	}
 }
 
-unsigned char buffer[16*1024];
+constexpr int buffer_size = 8*1024;
+unsigned char buffer[buffer_size];
+unsigned char buffer_next[buffer_size];
+
 
 int main()
 {
 	tick.attach(&audio_sample, 1.0 / sample_freq);
 	for(;;)
 	{
-//		bt.printf("hi");
-		pc.read (buffer, 16*1024, [](int x)
+		bt.read (buffer, buffer_size, [](int x)
 		    {
 			    bt.printf("reads\r\n");
-			    for (int i(0); i < 16*1024; i += 2)
+			    
+			    for (int i(0); i < buffer_size; i += 2)
 			    {
-				    int p = ((static_cast<int>(buffer[i]) << 8) | buffer[i+1]);
-				    bt.printf("%d ", p);
+				    int p = ((static_cast<int>(buffer_next[i]) << 8) | buffer_next[i+1]);
+				    pc.printf("%d ", p);
 			        data.push(p);
 			    }
 //			    pc.printf("data size %d\r\n", data.size());
