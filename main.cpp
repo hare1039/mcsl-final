0549a22315936edbd08518b25200437ba3531657
hare1039 <hare39393@gmail.com>
2018-08-08 18:21:36 +0800

    Finish up

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "mbed.h"
 #include "Serial.h"
 #include "PinNames.h"
+//#include "p.h"
 #include <queue>
 #include <string>
 
 
-constexpr float sample_freq = 16000.0;
+constexpr float sample_freq = 11200.0;
 
 Serial bt(D10, D2, 230400);
 Serial pc(USBTX, USBRX);
@@ -15,24 +16,26 @@ Ticker tick, tick2;
 constexpr int buffer_size = 19000; 
 std::queue<int> data;
 bool play_started = false;
+//static int x = 0;
 void audio_sample()
 {
-	if (data.size() > buffer_size || play_started)
-	{
-		speaker.write_u16(data.front());
-		data.pop();
-	    play_started = data.empty()? false: true;
-	}
+    if (data.size() > buffer_size || play_started)
+    {
+        speaker.write_u16(data.front());
+        data.pop();
+        play_started = data.empty()? false: true;
+    }
 } 
 
 int main()
 {
-	tick.attach(&audio_sample, 1.0 / sample_freq);
-	tick2.attach([](){pc.printf("data size: %d && start: %d\r\n", data.size(), play_started);}, 3.0 );
+    tick.attach(&audio_sample, 1.0 / sample_freq);
+    tick2.attach([](){pc.printf("data size: %d && start: %d\r\n", data.size(), play_started);}, 3.0 );
 
-	for (;;)
-	{
-		int i = bt.getc() << 8 | bt.getc();
-		data.push(i);
-	}
+    for (;;)
+    {
+        int i = bt.getc() << 8 | bt.getc();
+        data.push(i);
+//      pc.printf("%d", i);
+    }
 }
